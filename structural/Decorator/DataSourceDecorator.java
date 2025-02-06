8cc87c9a47c1503a1d0d060231534c19eaab0f8b
yaopengshuai <yaopengshuai@huice.com>
2025-02-06 13:36:32 +0800

    装饰模式

---
+++ structural/Decorator/DataSourceDecorator.java
@@ -0,0 +1,20 @@
+package Decorator;
+//抽象基础装饰
+public abstract class DataSourceDecorator implements DataSource
+{
+    private DataSource wrappee;
+
+    DataSourceDecorator(DataSource source) {
+        this.wrappee = source;
+    }
+
+    @Override
+    public void writeData(String data) {
+        wrappee.writeData(data);
+    }
+
+    @Override
+    public String readData() {
+        return wrappee.readData();
+    }
+}
